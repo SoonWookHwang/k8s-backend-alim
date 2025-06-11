87e28bc66f533cf0af8a14e6a8000ca02bc78f7a
SoonWookHwang <108779266+SoonWookHwang@users.noreply.github.com>
2025-06-11 17:29:57 +0900

    feign client 설정 추가

--- src/main/java/com/welab/k8sbackendalim/K8sBackendAlimApplication.java
+++ src/main/java/com/welab/k8sbackendalim/K8sBackendAlimApplication.java
@@ -2,8 +2,10 @@ package com.welab.k8sbackendalim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class K8sBackendAlimApplication {
 
     public static void main(String[] args) {
